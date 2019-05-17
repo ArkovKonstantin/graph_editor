bd84a953bf1a7bed05a071c7863d48979580ccd0
Arkov Konstantin <k.arkov@tcsbank.ru>
2019-05-17 19:06:25 +0300

    login/logout

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for, flash
 from app import app
 from app.forms import LoginForm, RegisterForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 
 @app.route('/')
@@ -12,31 +14,31 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
 
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.username.data).first()
-    #     if user:
-    #         if check_password_hash(user.password, form.password.data):
-    #             login_user(user, remember=form.remember.data)
-    #             return redirect(url_for('dashboard'))
-    #
-    #     return '<h1>Invalid username or password</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
 
-    return render_template('login.html', form=form)
+        login_user(user, remember=form.remeber.data)
+
+        return redirect(url_for("index"))
+
+    return render_template("login.html", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
-
-    # if form.validate_on_submit():
-    #     hashed_password = generate_password_hash(form.password.data, method='sha256')
-    #     new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-    #     db.session.add(new_user)
-    #     db.session.commit()
-    #
-    #     return '<h1>New user has been created!</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template('signup.html', form=form)
